327403399b1edd84d0495b9eff86fe69b094fba5
Pablo Barciela <scow@riseup.net>
2021-07-04 00:30:01 +0200

    rename mate -> cafe

--- autogen.sh
+++ autogen.sh
@@ -4,7 +4,7 @@
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-PKG_NAME="mate-themes"
+PKG_NAME="cafe-themes"
 
 (test -f $srcdir/configure.ac) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
@@ -12,8 +12,8 @@ PKG_NAME="mate-themes"
     exit 1
 }
 
-which mate-autogen || {
-    echo "You need to install mate-common from the MATE Git"
+which cafe-autogen || {
+    echo "You need to install cafe-common from the MATE Git"
     exit 1
 }
 
@@ -21,5 +21,5 @@ REQUIRED_AUTOMAKE_VERSION=1.9
 USE_MATE2_MACROS=1
 USE_COMMON_DOC_BUILD=yes
 
-. mate-autogen
+. cafe-autogen
 
